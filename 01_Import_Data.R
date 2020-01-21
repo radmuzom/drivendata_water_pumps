9c78763a85a0b9b68ac5ae52e981089590a198d5
Anindya Mozumdar <radmuzom@users.noreply.github.com>
2020-01-21 23:04:31 +0800

    Remove redundant columns. Convert all values to lower case.

--- 01_Import_Data.R
+++ 01_Import_Data.R
@@ -16,3 +16,27 @@ names(train_values)
 # Combine the label and values
 train <- merge(train_values, train_labels, by = "id")
 dim(train)
+
+# Drop the original values and labels
+rm(train_labels)
+rm(train_values)
+
+# Remove columns which cannot be used as features
+dropcols <- c("date_recorded", "wpt_name", "num_private", "recorded_by", "id")
+train <- train[, -..dropcols]
+
+# Type of each column
+sapply(train, class)
+
+# Convert region_code and district_code to character
+train[["region_code"]] <- as.character(train[["region_code"]])
+train[["district_code"]] <- as.character(train[["district_code"]])
+
+# Convert all character values to lower case
+train <- as.data.table(lapply(train, function(v) {
+  if (is.character(v)) {
+    return(tolower(v))
+  } else {
+    return(v)
+  }
+}))
