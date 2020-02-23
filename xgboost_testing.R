e618fd42601416aceb90898d7edac1540e16045e
Anindya Mozumdar <radmuzom@users.noreply.github.com>
2020-02-23 20:41:49 +0800

    Implemented Bayesian search of hyperparameters.

--- xgboost_testing.R
+++ xgboost_testing.R
@@ -1,4 +1,5 @@
 library(xgboost)
+library(rBayesianOptimization)
 
 train <- train_values[, c(1, 2, 33:70)]
 train[["Random"]] <- runif(nrow(train))
@@ -16,25 +17,9 @@ train_train_mat <- xgb.DMatrix(data = as.matrix(train_train),
 train_test_mat <- xgb.DMatrix(data = as.matrix(train_test),
                               label = train_test_label)
 
-search_grid <- expand.grid(
-  eta = seq(0.03, 0.3, by = 0.03),
-  gamma = c(0, 1, 2, 3, 4, 5),
-  max_depth = c(3, 4, 5, 6, 7, 8),
-  min_child_weight = seq(25, 200, by = 25),
-  subsample = seq(0.4, 1, by = 0.1)
-)
-
-# setDT(search_grid)
-# set.seed(NULL)
-# search_grid <- search_grid[sample(.N, 20), ]
-
-hp_search <- apply(search_grid, 1, function(param_list) {
-  c_eta <- param_list[["eta"]]
-  c_gamma <- param_list[["gamma"]]
-  c_max_depth <- param_list[["max_depth"]]
-  c_min_child_weight <- param_list[["min_child_weight"]]
-  c_subsample <- param_list[["subsample"]]
-  
+cv_folds <- KFold(train_train_label, nfolds = 5, stratified = TRUE, seed = 0)
+xgb_cv_bayes <- function(eta, gamma, max_depth,
+                         min_child_weight, subsample, nrounds) {
   param_list <- list(
     "booster" = "gbtree",
     "verbosity" = 3,
@@ -43,32 +28,52 @@ hp_search <- apply(search_grid, 1, function(param_list) {
     "objective" = "multi:softprob",
     "eval_metric" = "merror",
     "num_class" = 3,
-    "eta" = c_eta,
-    "gamma" = c_gamma,
-    "max_depth" = c_max_depth,
-    "min_child_weight" = c_min_child_weight,
-    "subsample" = c_subsample
+    "nrounds" = nrounds,
+    "eta" = eta,
+    "gamma" = gamma,
+    "max_depth" = max_depth,
+    "min_child_weight" = min_child_weight,
+    "subsample" = subsample
   )
   
   xgcv <- xgb.cv(
     params = param_list,
     data = train_train_mat,
-    nrounds = 300,
+    nround = 1000,
+    folds = cv_folds,
+    prediction = TRUE,
+    showsd = TRUE,
     early_stopping_rounds = 10,
-    nfold = 5
+    verbose = 0
   )
-  
+
   validation_scores <- as.data.frame(xgcv$evaluation_log)
   train_error <- tail(validation_scores$train_merror_mean, 1)
   test_error <- tail(validation_scores$test_merror_mean, 1)
-  c(c_eta, c_gamma, c_max_depth, c_min_child_weight, c_subsample,
-    train_error, test_error)
-})
+  list(Score = -test_error,
+       Pred = xgcv$pred)
+}
 
-hp_search <- as.data.frame(t(hp_search))
-colnames(hp_search) <- c("eta", "gamma", "max_depth", "min_child_weight",
-                         "subsample", "train_error", "test_error")
-hp_search <- hp_search[order(hp_search$test_error), ]
+opt_res <- BayesianOptimization(
+  xgb_cv_bayes,
+  bounds = list(
+    eta = c(0.03, 0.3),
+    gamma = c(0, 50),
+    max_depth = c(2L, 8L),
+    min_child_weight = c(25L, 200L),
+    subsample = c(0.5, 0.9),
+    nrounds = c(50L, 1000L)
+  ),
+  init_grid_dt = NULL,
+  init_points = 10,
+  n_iter = 100,
+  acq = "ucb",
+  kappa = 2.576,
+  eps = 0.0,
+  verbose = TRUE
+)
+
+# Modify the parameters below using the Bayesian search results
 
 param_list <- list(
   "booster" = "gbtree",
@@ -78,11 +83,11 @@ param_list <- list(
   "objective" = "multi:softprob",
   "eval_metric" = "merror",
   "num_class" = 3,
-  "eta" = hp_search[1, 1],
-  "gamma" = hp_search[1, 2],
-  "max_depth" = hp_search[1, 3],
-  "min_child_weight" = hp_search[1, 4],
-  "subsample" = hp_search[1, 5]
+  "eta" = 0.3,
+  "gamma" = 0,
+  "max_depth" = 8,
+  "min_child_weight" = 25,
+  "subsample" = 0.9
 )
 bst <- xgb.train(params = param_list, data = train_train_mat, nrounds = 300)
 test_pred <- predict(bst, newdata = train_test_mat)
